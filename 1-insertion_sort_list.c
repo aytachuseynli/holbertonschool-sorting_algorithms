af15685d8ebef1d37200cdbb450ab95a8345d44d
aytachuseynli <aytac_ha@mail.ru>
2024-01-17 02:17:54 -0800

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,35 +7,34 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *temp;
+    listint_t *current, *temp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	current = (*list)->next;
+    current = (*list)->next;
 
-	while (current != NULL)
-	{
-		temp = current->next;
+    while (current != NULL)
+    {
+        temp = current->next;
 
-		while (current->prev != NULL && current->n < current->prev->n)
-		{
-			if (current->next != NULL)
-				current->next->prev = current->prev;
-			current->prev->next = current->next;
+        while (current->prev != NULL && current->n < current->prev->n)
+        {
+            if (current->next != NULL)
+                current->next->prev = current->prev;
+            current->prev->next = current->next;
 
-			current->next = current->prev;
-			current->prev = current->prev->prev;
+            current->next = current->prev;
+            current->prev = current->prev->prev;
 
-			if (current->prev != NULL)
-				current->prev->next = current;
+            if (current->prev != NULL)
+                current->prev->next = current;
+            else
+                *list = current;
 
-			if (current->next == NULL)
-				*list = current;
+            print_list(*list);
+        }
 
-			print_list(*list);
-		}
-
-		current = temp;
-	}
+        current = temp;
+    }
 }
