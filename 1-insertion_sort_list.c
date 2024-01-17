a328dcf65610f6c96d7315f025cdd5873dda8063
aytachuseynli <aytac_ha@mail.ru>
2024-01-17 12:08:55 -0800

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,5 +7,27 @@
  */
 void insertion_sort_list(listint_t **list)
 {
+    if (!list || !*list || !(*list)->next)
+        return;
 
+    listint_t *temp, *current = (*list)->next;
+
+    while (current)
+    {
+        temp = current->next;
+
+        while (current->prev && current->n < current->prev->n)
+        {
+            current->prev->next = current->next;
+            if (current->next)
+                current->next->prev = current->prev;
+            current->next = current->prev;
+            current->prev = current->prev->prev;
+            current->next->prev = current;
+            *list = current->prev ? *list : current;
+            print_list(*list);
+        }
+
+        current = temp;
+    }
 }
