432870732f3fe89c2c170bda89e5323fde054523
aytachuseynli <aytac_ha@mail.ru>
2024-01-20 10:25:44 -0800

    quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,1 +1,76 @@
+#include "sort.h"
 
+/**
+ * lomuto_partition - Lomuto partition scheme for Quick Sort
+ * @array: Array to be partitioned
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ *
+ * Return: Index of the pivot element after partitioning
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int i = low - 1, j, temp;
+
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+
+			if (i != j)
+			{
+				print_array(array, size);
+			}
+		}
+	}
+
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+
+	if (i + 1 != high)
+	{
+		print_array(array, size);
+	}
+
+	return (i + 1);
+}
+
+/**
+ * quick_sort_recursive - Recursive function for Quick Sort
+ * @array: Array to be sorted
+ * @low: Starting index of the partition
+ * @high: Ending index of the partition
+ * @size: Size of the array
+ */
+void quick_sort_recursive(int *array, int low, int high, size_t size)
+{
+	int partition;
+
+	if (low < high)
+	{
+		partition = lomuto_partition(array, low, high, size);
+
+		quick_sort_recursive(array, low, partition - 1, size);
+		quick_sort_recursive(array, partition + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order using Quick sort
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quick_sort_recursive(array, 0, size - 1, size);
+}
